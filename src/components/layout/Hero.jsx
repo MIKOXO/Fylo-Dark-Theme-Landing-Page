67e317ea51f7dbc9d76a6b55c0300c52bb7cf7f7
MIKOXO <mikermount19@yahoo.com>
2025-09-12 16:48:45 +0300

    Hero section added

---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import heroImage from "../../assets/illustration-intro.png";
+
+const Hero = () => {
+  return (
+    <section className="mx-auto container p-6 mt-5 lg:mt-14">
+      <div className="flex flex-col items-center">
+        {/* Image */}
+        <div>
+          <img src={heroImage} alt="illustration-intro" />
+        </div>
+
+        {/* Text Content */}
+        <div className="text-center mt-10 max-w-2xl text-White">
+          <h1 className="font-Raleway font-bold text-[24px] sm:text-[36px] mb-6 max-w-[700px] mx-auto">
+            All your files in one secure location, accessible anywhere.
+          </h1>
+          <p className="font-OpenSans max-w-[500px] mx-auto max-sm:text-[14px]">
+            Fylo stores all your most important files in one secure location.
+            Access them wherever you need, share and collaborate with friends,
+            family and co-workers.
+          </p>
+          <button className="font-OpenSans font-semibold px-8 py-3 rounded-full w-[250px] text-White bg-gradient-to-r from-Teal200 to-Cyan500 my-4">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/layout/Navbar";
+import Hero from "../components/layout/Hero";
 
 const Home = () => {
   return (
     <>
       <main className="bg-Navy850 w-full min-h-screen">
         <Navbar />
+        <Hero />
       </main>
     </>
   );
